5702e602e87c78471c728dc584bfef8feca9903f
unknown <thilobarth@hotmail.de>
2018-12-17 22:28:35 +0100

    Grundgerüst

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,46 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+var FOODGEN = {
+    food: [
+        "Hotdog",
+        "Burger",
+        "Döner",
+        "Pizza"
+    ],
+    init: function(){
+        console.log("Das Programm ist fertig geladen!");
     },
+    gen: function(){
+        //Zufällige Zahl generieren
+        rand = Math.floor(Math.random() * this.food.length);
+        index = this.food.indexOf(this.food[rand]);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        if(this.food.length == 0){
+            alert("Du ficker hast keine essen mehr zur verfügung");
+        } else {
+            alert("Dein essen des heutigen Tages ist: " + this.food[rand]);
+        }
+        console.log(this.food);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        //Das Schon gezogene essen aus dem Array löschen
+        if (index > -1) {
+            this.food.splice(index, 1);
+        }
+        console.log(this.food);
     }
 };
 
-app.initialize();
+FOODGEN.init();
+
+document.getElementById("randome").onclick = function() {FOODGEN.gen()};
